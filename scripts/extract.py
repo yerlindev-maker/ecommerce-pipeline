a11ed3b06837821c2a37e0fe29fc08535d713fe1
yerlindev-maker <yerlindev@gmail.com>
2026-06-13 20:26:01 -0500

    extract return response.json()

--- scripts/extract.py
+++ scripts/extract.py
@@ -4,5 +4,5 @@ def fetch_products():
     URL = "https://fakestoreapi.com/products"
     response = requests.get(URL)
     response.raise_for_status()
-    return response
+    return response.json()
 
